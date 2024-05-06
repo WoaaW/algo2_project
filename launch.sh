990ff8246a9137340e0ea08192fb9d0f44bb7db8
Thibault Maucq <thibault.maucq@ulb.be>
2024-05-06 17:15:42 +0200

    Commentaire script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,17 @@
+:"
+Projet Algorithmique 2 - FIAT LUX
+
+Auteurs : Maucq Thibault (000 47 49 22) et Van Sint Jan Kolya (000 57 37 39)
+
+Description : Script launch du projet d'algorithmique 2. Ce script compile tous les fichiers utiles à l'exécution
+              des algorithmes. Une fois la compilation terminée, le script lance l'exécutable avec les arguments
+              donnés par l'utilisateur. Ici $1 peut être soit -d (décision) soit -o (optimisation) pour sélectionner
+              l'algorithme à exécuter et $2 correspond au nom du fichier .txt (test.txt par exemple) où les
+              informations de la grille sont stockées.
+
+Date : 6 mai 2024.
+"
+
 cd src/main
 javac Main.java -classpath ../classes/ -d ../../build/
 
